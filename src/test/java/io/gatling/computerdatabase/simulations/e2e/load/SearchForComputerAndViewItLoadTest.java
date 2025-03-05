ef6f8d0280af2f5ce766aec3a754d2ae6d6e6e2e
Maciej Fec <Maciej.Fec@2itesting.com>
2025-03-05 19:21:58 +0000

    fix mistake

--- src/test/java/io/gatling/computerdatabase/simulations/e2e/load/SearchForComputerAndViewItLoadTest.java
+++ src/test/java/io/gatling/computerdatabase/simulations/e2e/load/SearchForComputerAndViewItLoadTest.java
@@ -9,20 +9,19 @@ import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
-import static io.gatling.javaapi.core.CoreDsl.feed;
+import static java.time.temporal.ChronoUnit.*;
 
 public class SearchForComputerAndViewItLoadTest extends Simulation {
 
     public static class LoadProfile {
         private static final int TOTAL_LOAD = 10;
         private static final int USER_RAMP_UP_PER_INTERVAL = 1;
-        private static final Duration TOTAL_RAMP_UP_TIME = Duration.of(20, ChronoUnit.SECONDS);
-        private static final Duration STEADY_STATE_DURATION = Duration.of(40, ChronoUnit.SECONDS);
+        private static final Duration TOTAL_RAMP_UP_TIME = Duration.of(20, SECONDS);
+        private static final Duration STEADY_STATE_DURATION = Duration.of(40, SECONDS);
 
-        public static ClosedInjectionStep[] generate() {
+        public static ClosedInjectionStep[] generateLoad() {
             return new ClosedInjectionStep[]{
                     rampConcurrentUsers(USER_RAMP_UP_PER_INTERVAL).to(TOTAL_LOAD).during(TOTAL_RAMP_UP_TIME),
                     constantConcurrentUsers(TOTAL_LOAD).during(STEADY_STATE_DURATION)
@@ -40,6 +39,6 @@ public class SearchForComputerAndViewItLoadTest extends Simulation {
             .exec(feed(feeder), openComputersPage, searchForComputer, openComputer);
 
     {
-        setUp(searchAndOpenScn.injectClosed(LoadProfile.generate())).protocols(httpProtocol);
+        setUp(searchAndOpenScn.injectClosed(LoadProfile.generateLoad())).protocols(httpProtocol);
     }
 }
